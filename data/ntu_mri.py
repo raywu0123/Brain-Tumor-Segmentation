9dc6cafb48e215a14328bfae45f9f6e9164f297e
raywu0123 <b05901189@ntu.edu.tw>
2018-10-01 10:35:53 -0400

    fix data dim format

--- data/ntu_mri.py
+++ data/ntu_mri.py
@@ -35,6 +35,10 @@ class NTU_MRI(DataInterface):
         label_path = os.path.join(self.label_path, data_id)
         image = nib.load(img_path).get_fdata()
         label = nib.load(label_path).get_fdata()
+        
+        # Dims: (N, C, D, H, W)
+        image = np.transpose(image, (2, 0, 1))
+        label = np.transpose(label, (2, 0, 1))
         return image, label
 
     def _data_generator(self, data_ids, batch_size):
