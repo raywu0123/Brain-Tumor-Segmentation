ef596f86e2d153966b5347414fac7147a2f0e4c4
harry1003 <b05901059@ntu.edu.tw>
2018-10-14 13:05:24 +0800

    UNet can run

--- models/UNet.py
+++ models/UNet.py
@@ -3,53 +3,84 @@ import torch.nn as nn
 import torch.nn.functional as F
 from .base import Model2DBase
 from dotenv import load_dotenv
+import torch.optim as optim
 load_dotenv('./.env')
 
 
 class UNet(Model2DBase):
     def __init__(
+            self,
+            channels: int = 1,
+            depth: int = 200,
+            height: int = 200,
+            width: int = 200,
+            metadata_dim: int = 0,
+            num_units: [int] = (32, 32, 64, 64, 128),
+            pooling_layer_num: [int] = (1, 3),
+            kernel_size: int = 3,
+            lr: float = 1e-4,
+        ):
+        super(UNet, self).__init__(
+            channels=channels,
+            depth=depth,
+            height=height,
+            width=width,
+            metadata_dim=metadata_dim,
+        )
+        self.model = UNet_structure(
+            image_chns=self.data_channels,
+            kernel_size=kernel_size,
+            floor_num=2,
+            channel_num=48,
+        )
+        self.opt = optim.Adam(params=self.model.parameters(), lr=lr)
+        if torch.cuda.is_available():
+            self.model.cuda()
+
+
+class UNet_structure(nn.Module):
+    def __init__(
         self,
-        channels: int = 1,
-        kernel_size: int = 3,
-        channel_num: int = 48,
-        floor_num: int = 4,
+        image_chns,
+        floor_num,
+        kernel_size,
+        channel_num,
     ):
-        super(UNet, self).__init__()
+        super(UNet_structure, self).__init__()
         self.floor_num = floor_num
+        self.image_chns = image_chns
         self.down_layers = nn.ModuleList()
         self.up_layers = nn.ModuleList()
 
-        self.inc = inconv(channels, channel_num, kernel_size)
-
-        for i in range(floor_num):
-            d = down(channel_num, channel_num * 2, kernel_size)
-            channel_num = int(channel_num * 2)
+        self.inc = inconv(image_chns, channel_num, kernel_size)
+        for floor in range(floor_num):
+            a = 2 ** floor
+            d = down(channel_num * a, channel_num * a * 2, kernel_size)
             self.down_layers.append(d)
+        a = 2 ** floor_num
+        self.down_end = down(channel_num * a, channel_num * a, kernel_size)
 
-        channel_num = channel_num * 2
-        for i in range(floor_num):
-            u = up(channel_num, int(channel_num / 2), kernel_size)
-            channel_num = int(channel_num / 2)
+        for floor in range(floor_num):
+            f_b = floor_num - floor - 1
+            a = 2 ** f_b
+            u = up(channel_num * 4 * a, channel_num * a, kernel_size)
             self.up_layers.append(u)
-
-        self.outc = outconv(channel_num)
+        self.up_end = up(channel_num * 2, channel_num * 1, kernel_size)
+        self.outc = outconv(channel_num, image_chns)
 
     def forward(self, x):
         x_out = []
-
         x = self.inc(x)
         x_out.append(x)
-
-        for i in range(self.floor_num):
-            x = self.down_layers[i](x)
+        for d in (self.down_layers):
+            x = d(x)
             x_out.append(x)
-
-        for i in range(self.floor_num):
-            x = self.up_layers[i](x, x_out[self.floor_num - 1 - i])
-
+        x = self.down_end(x)
+        for floor, u in enumerate(self.up_layers):
+            f_b = self.floor_num - floor
+            x = u(x, x_out[f_b])
+        x = self.up_end(x, x_out[0])
         x = self.outc(x)
-
-        x = torch.sigmoid(x)
         return x
 
 
@@ -60,7 +91,7 @@ class double_conv(nn.Module):
             nn.Conv2d(in_ch, out_ch, kernel_size, padding=1),
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_ch, out_ch, kernel_size, padding=1),
+            nn.ConvTranspose2d(out_ch, out_ch, kernel_size, padding=1),
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True)
         )
@@ -115,9 +146,9 @@ class up(nn.Module):
 
 
 class outconv(nn.Module):
-    def __init__(self, in_ch):
+    def __init__(self, in_ch, out_ch):
         super(outconv, self).__init__()
-        self.conv = nn.Conv2d(in_ch, 1, 1)
+        self.conv = nn.Conv2d(in_ch, out_ch, 1)
 
     def forward(self, x):
         x = self.conv(x)
