fix two-dim sampler & center-patch-sampler & fix normalization

models/base.py
@@ -3,21 +3,11 @@ from abc import ABC, abstractmethod
 import torch
 from torch import nn
 
-from .batch_samplers.two_dim import TwoDimBatchSampler
-from .batch_samplers.three_dim import ThreeDimBatchSampler
-from .batch_samplers.patch3d import Patch3DBatchSampler
+from .batch_samplers import BatchSamplerHub
 from .utils import summarize_logs
 
 
 class ModelBase(ABC):
-
-    def __init__(self, batch_sampler_id: str):
-        sampler_hub = {
-            'two_dim': TwoDimBatchSampler,
-            'three_dim': ThreeDimBatchSampler,
-            'patch3d': Patch3DBatchSampler,
-        }
-        self.batch_sampler = sampler_hub[batch_sampler_id]()
 
     @abstractmethod
     def fit_generator(self, training_data_generator, optimizer, **kwargs):
@@ -32,11 +22,17 @@ class PytorchModelBase(ModelBase, nn.Module):
 
     def __init__(self, batch_sampler_id: str, loss_fn):
         nn.Module.__init__(self)
-        ModelBase.__init__(self, batch_sampler_id=batch_sampler_id)
         self.loss_fn = loss_fn
+        self.batch_sampler_constructor = BatchSamplerHub[batch_sampler_id]
+        self.batch_sampler = None
 
     def fit_generator(self, training_data_generator, optimizer, **kwargs):
         self.train()
+        if not self.batch_sampler:
+            self.batch_sampler = self.batch_sampler_constructor(
+                data_format=training_data_generator.data_format
+            )
+
         data = training_data_generator(batch_size=1)
         batch_data_list, batch_label_list = self.batch_sampler.convert_to_feedable(
             data, training=True, **kwargs

models/batch_samplers/center_patch3d.py
@@ -1,4 +1,5 @@
 from math import ceil
+import random
 
 import numpy as np
 
@@ -7,9 +8,12 @@ from preprocess_tools.image_utils import crop_or_pad_to_shape
 from .utils import flatten
 
 
-class Patch3DBatchSampler(BatchSamplerBase):
+class CenterPatch3DBatchSampler(BatchSamplerBase):
 
-    patch_size = (64, 64, 64)
+    patch_size = np.array((64, 64, 64), dtype=int)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def convert_to_feedable(self, batch_data, batch_size, training=False, **kwargs):
         batch_volume = batch_data['volume']
@@ -18,9 +22,10 @@ class Patch3DBatchSampler(BatchSamplerBase):
         feedable_data_list = []
         feedable_label_list = []
 
-        index_lists = self._generate_index_lists(batch_volume.shape, random=training)
-        batch_index_lists = np.array_split(index_lists, ceil(len(index_lists) / batch_size))
-        for batch_indexes in batch_index_lists:
+        index_list = self._generate_index_list(batch_label, random=training)
+        index_lists = np.array_split(index_list, ceil(len(index_list) / batch_size))
+
+        for batch_indexes in index_lists:
             batch_patch_volume, batch_patch_label = \
                 self._sample_by_batch_lists(batch_volume, batch_label, batch_indexes)
             feedable_data_list.append(batch_patch_volume)
@@ -48,21 +53,16 @@ class Patch3DBatchSampler(BatchSamplerBase):
         patch = crop_or_pad_to_shape(patch, [patch.shape[0], *self.patch_size])
         return patch
 
-    def _generate_index_lists(self, volume_shape, random):
-        lists = []
+    def _generate_index_list(self, batch_label, random):
+        volume_shape = batch_label.shape
         if random:
             num_patches = volume_shape[0] \
                 * ceil(volume_shape[2] / self.patch_size[0]) \
                 * ceil(volume_shape[3] / self.patch_size[1]) \
                 * ceil(volume_shape[4] / self.patch_size[2])
-
-            for idx in range(num_patches):
-                n = np.random.randint(0, volume_shape[0])
-                d = np.random.randint(0, volume_shape[2] - self.patch_size[0])
-                h = np.random.randint(0, volume_shape[3] - self.patch_size[1])
-                w = np.random.randint(0, volume_shape[3] - self.patch_size[2])
-                lists.append([n, d, h, w])
+            return self._generate_index_list_around_label(batch_label, num_patches)
         else:
+            lists = []
             for n in range(volume_shape[0]):
                 for d in range(ceil(volume_shape[2] / self.patch_size[0])):
                     for h in range(ceil(volume_shape[3] / self.patch_size[1])):
@@ -73,7 +73,38 @@ class Patch3DBatchSampler(BatchSamplerBase):
                                 h * self.patch_size[1],
                                 w * self.patch_size[2],
                             ])
+            return lists
+
+    def _generate_index_list_around_label(self, batch_label, num_patches):
+        lists = []
+        random_n_list = np.random.randint(0, len(batch_label), size=num_patches)
+        indexes_dict = {}
+        for random_n in random_n_list:
+            if random_n not in indexes_dict:
+                selected_label = batch_label[random_n]
+                # find label except background
+                selected_label = np.sum(selected_label[1:], axis=0)
+                selected_label = (selected_label != 0)
+                indexes = np.stack(np.where(selected_label), axis=-1)
+                indexes_dict[random_n] = indexes
+            else:
+                indexes = indexes_dict[random_n]
+            selected_label_position = random.choice(indexes)
+            patch_position = self._get_patch_position(selected_label_position)
+            selected_label_position = [random_n, *patch_position]
+            lists.append(selected_label_position)
+
         return lists
+
+    def _get_patch_position(self, selected_label_position):
+        pos = []
+        for patch_l, label_pos, data_l in zip(
+                self.patch_size, selected_label_position, self.data_format
+        ):
+            x = label_pos - np.random.randint(0, patch_l)
+            x = np.clip(x, a_min=0, a_max=data_l - patch_l)
+            pos.append(x)
+        return pos
 
     def reassemble(self, prediction: list, test_data: dict, **kwargs):
         test_volume = test_data['volume']
@@ -82,7 +113,7 @@ class Patch3DBatchSampler(BatchSamplerBase):
             prediction[0].shape[1],
             *test_volume.shape[2:],
         ])
-        index_lists = self._generate_index_lists(test_volume.shape, random=False)
+        index_lists = self._generate_index_list(test_volume, random=False)
         prediction = flatten(prediction)
 
         for pred_patch, index_list in zip(prediction, index_lists):

models/batch_samplers/tests/test.py
@@ -3,7 +3,8 @@ from unittest import TestCase
 import numpy as np
 
 from ..two_dim import TwoDimBatchSampler
-from ..patch3d import Patch3DBatchSampler
+from ..uniform_patch3d import UniformPatch3DBatchSampler
+from ..center_patch3d import CenterPatch3DBatchSampler
 
 
 class TwoDimBatchSamplerTestCase(TestCase):
@@ -11,7 +12,15 @@ class TwoDimBatchSamplerTestCase(TestCase):
     def setUp(self):
         self.batch_volume = np.random.random([10, 9, 8, 7, 6])
         self.batch_label = np.random.randint(2, size=[10, 2, 8, 7, 6])
-        self.sampler = TwoDimBatchSampler()
+        self.sampler = TwoDimBatchSampler(
+            data_format={
+                'channels': 9,
+                'depth': 8,
+                'height': 7,
+                'width': 6,
+                'class_num': 2,
+            }
+        )
         self.batch_size = 2
 
     def test_reverse(self):
@@ -28,17 +37,52 @@ class TwoDimBatchSamplerTestCase(TestCase):
 class Patch3DBatchSamplerTestCase(TestCase):
 
     def setUp(self):
-        self.batch_volume = np.random.random([5, 9, 64, 128, 200])
-        self.batch_label = np.random.randint(2, size=[5, 2, 64, 128, 200])
-        self.sampler = Patch3DBatchSampler()
+        self.data_format = {
+            'channels': 5,
+            'depth': 64,
+            'height': 128,
+            'width': 200,
+            'class_num': 2,
+        }
+        self.n_data = 5
+        self.batch_volume = np.random.random([
+            self.n_data,
+            self.data_format['channels'],
+            self.data_format['depth'],
+            self.data_format['height'],
+            self.data_format['width']]
+        )
+        self.batch_label = np.random.choice(
+            [0, 1],
+            size=[
+                self.n_data,
+                self.data_format['class_num'],
+                self.data_format['depth'],
+                self.data_format['height'],
+                self.data_format['width'],
+            ],
+            p=[0.01, 0.99],
+        )
+        self.batch_data = {'volume': self.batch_volume, 'label': self.batch_label}
+
+        self.uniform_sampler = UniformPatch3DBatchSampler(data_format=self.data_format)
+        self.center_sampler = CenterPatch3DBatchSampler(data_format=self.data_format)
+
         self.batch_size = 3
 
     def test_reverse(self):
-        batch_data = {'volume': self.batch_volume, 'label': self.batch_label}
-        _, batch_label_list = self.sampler.convert_to_feedable(
-            batch_data, batch_size=self.batch_size
+        _, batch_label_list = self.uniform_sampler.convert_to_feedable(
+            self.batch_data, batch_size=self.batch_size, training=False
         )
-        reversed_batch_label = self.sampler.reassemble(batch_label_list, batch_data)
+        reversed_batch_label = self.uniform_sampler.reassemble(batch_label_list, self.batch_data)
         self.assertTrue(
             np.all(reversed_batch_label == self.batch_label)
         )
+
+    def test_center_sampler(self):
+            _, batch_label_list = self.uniform_sampler.convert_to_feedable(
+                self.batch_data, batch_size=self.batch_size, training=True
+            )
+            _, batch_label_list = self.center_sampler.convert_to_feedable(
+                self.batch_data, batch_size=self.batch_size, training=True
+            )

models/batch_samplers/two_dim.py
@@ -9,6 +9,9 @@ from ..utils import co_shuffle
 
 class TwoDimBatchSampler(BatchSamplerBase):
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
     def convert_to_feedable(self, batch_data, batch_size, training=False, **kwargs):
         volume = batch_data['volume']
         label = batch_data['label']
@@ -28,8 +31,7 @@ class TwoDimBatchSampler(BatchSamplerBase):
         return feedable_data_list, feedable_label_list
 
     def reassemble(self, preds: list, test_data: dict, **kwargs):
-        all_segmentations = np.asarray([pred for pred in preds])
-        all_segmentations = all_segmentations.reshape(-1, *all_segmentations.shape[2:])
+        all_segmentations = np.concatenate(preds, axis=0)
         data_depth = test_data['volume'].shape[2]
         volume_segmentations = get_3d_from_2d(all_segmentations, data_depth)
         return volume_segmentations

models/loss_functions.py
@@ -7,32 +7,25 @@ epsilon = 1e-8
 
 
 def ce_minus_log_dice(pred: torch.Tensor, tar: np.array):
-
-    channel_num = tar.shape[1]
-    temp = np.swapaxes(tar, 0, 1).reshape(channel_num, -1)
-    class_weights = np.divide(
-        1., np.mean(temp, axis=(1)),
-        out=np.ones(tar.shape[1]),
-        where=np.mean(temp, axis=(1)) != 0,
-    )
-
-    tar = get_tensor_from_array(tar)
-    crossentropy_loss, log_1 = weighted_cross_entropy(pred, tar, weights=class_weights)
+    crossentropy_loss, log_1 = weighted_cross_entropy(pred, tar)
     dice_score, log_2 = soft_dice_score(pred, tar)
     total_loss = crossentropy_loss - torch.log(dice_score)
     return total_loss, {**log_1, **log_2}
 
 
-def weighted_cross_entropy(output: torch.Tensor, target: torch.Tensor, weights=None):
+def weighted_cross_entropy(output: torch.Tensor, target: np.array):
     assert(output.shape == target.shape)
-    if weights is None:
-        weights = (1,) * output.shape[1]
-    else:
-        assert(len(weights) == output.shape[1])
 
-    weights = torch.Tensor(weights)
-    if torch.cuda.is_available():
-        weights = weights.cuda()
+    channel_num = target.shape[1]
+    temp = np.swapaxes(target, 0, 1).reshape(channel_num, -1)
+    weights = np.divide(
+        1., np.mean(temp, axis=1),
+        out=np.ones(target.shape[1]),
+        where=np.mean(temp, axis=1) != 0,
+    )
+
+    weights = get_tensor_from_array(weights)
+    target = get_tensor_from_array(target)
 
     target = target.transpose(1, -1)
     output = output.transpose(1, -1)
@@ -41,12 +34,14 @@ def weighted_cross_entropy(output: torch.Tensor, target: torch.Tensor, weights=N
     return loss, {'crossentropy_loss': loss.item()}
 
 
-def soft_dice_score(pred: torch.Tensor, tar: torch.Tensor):
+def soft_dice_score(pred: torch.Tensor, tar: np.array):
     if not pred.shape == tar.shape:
         raise ValueError(f'Shape mismatch in pred and tar, got {pred.shape} and {tar.shape}')
     if not pred.shape[1] > 1:
         raise ValueError(f'Number of channels should be greater than 1, '
                          f'got data with shape {pred.shape}')
+    tar = get_tensor_from_array(tar)
+
     # Strip background
     pred = pred[:, 1:]
     tar = tar[:, 1:]

models/v_net.py
@@ -1,14 +1,9 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 from .base import PytorchModelBase
-from .loss_functions import ce_minus_log_dice
+from .loss_functions import weighted_cross_entropy
 from .utils import get_tensor_from_array
-
-
-def activation_fn():
-    return nn.ReLU()
 
 
 class VNet(PytorchModelBase):
@@ -16,20 +11,19 @@ class VNet(PytorchModelBase):
     def __init__(
             self,
             data_format: dict,
-            duplication_num: int = 8,
-            kernel_size: int = 5,
+            duplication_num: int = 16,
+            kernel_size: int = 3,
             conv_time: int = 2,
-            n_layer: int = 4,
+            n_layer: int = 3,
             batch_sampler_id='three_dim',
         ):
         super(VNet, self).__init__(
             batch_sampler_id=batch_sampler_id,
-            loss_fn=ce_minus_log_dice,
+            loss_fn=weighted_cross_entropy,
         )
         # To work properly, kernel_size must be odd
         if kernel_size % 2 == 0:
             raise AssertionError('kernel_size({}) must be odd'.format(kernel_size))
-        self.n_layer = n_layer
 
         self.down = nn.ModuleList()
         self.up = nn.ModuleList()
@@ -42,13 +36,13 @@ class VNet(PytorchModelBase):
 
         for i in range(n_layer - 1):
             n_channel = (2 ** i) * duplication_num
-            up_conv = UpConv(n_channel * 4, n_channel, kernel_size, conv_time)
+            up_conv = UpConv(n_channel * 2, n_channel, kernel_size, conv_time)
             self.up.append(up_conv)
 
         n_channel = (2 ** (n_layer - 1)) * duplication_num
         up_conv = UpConv(n_channel * 2, n_channel, kernel_size, conv_time)
         self.up.append(up_conv)
-        self.output_layer = OutLayer(duplication_num * 2, data_format['class_num'])
+        self.output_layer = OutLayer(duplication_num, data_format['class_num'])
 
     def forward(self, x):
         x = get_tensor_from_array(x)
@@ -84,17 +78,15 @@ class DownConv(nn.Module):
     def __init__(self, input_channel, kernel_size, conv_time):
         super(DownConv, self).__init__()
         output_channel = input_channel * 2
-        self.down_conv = nn.Conv3d(input_channel, output_channel, kernel_size=2, stride=2)
-        self.activation = activation_fn()
+        self.down_conv = nn.Conv3d(input_channel, output_channel, kernel_size=kernel_size, stride=2)
         self.batch_norm = nn.BatchNorm3d(output_channel)
         self.conv_N_time = ConvNTimes(output_channel, kernel_size, conv_time)
 
     def forward(self, x):
         x = self.down_conv(x)
-        x = self.activation(x)
-        x1 = self.batch_norm(x)
-        x = self.conv_N_time(x1)
-        x = x + x1
+        x = self.batch_norm(x)
+        x = F.relu(x)
+        x = self.conv_N_time(x)
         return x
 
 
@@ -108,17 +100,16 @@ class UpConv(nn.Module):
 
     def __init__(self, x1_channel, x2_channel, kernel_size, conv_time):
         super(UpConv, self).__init__()
-        self.up_conv = nn.ConvTranspose3d(x1_channel, x2_channel, kernel_size=2, stride=2)
-        self.activation = activation_fn()
+        self.up_conv = nn.ConvTranspose3d(x1_channel, x2_channel, kernel_size=kernel_size, stride=2)
         self.batch_norm = nn.BatchNorm3d(x2_channel)
-        self.conv_N_time = ConvNTimes(x2_channel * 2, kernel_size, conv_time)
+        self.conv_N_time = ConvNTimes(x2_channel, kernel_size, conv_time)
 
     def forward(self, x1, x2):
         x1 = self.up_conv(x1)
-        x1 = self.activation(x1)
         x1 = self.batch_norm(x1)
+        x1 = F.relu(x1)
         if x1.shape != x2.shape:
-            # this case will only happend for
+            # this case will only happen when
             # x1 [N, C, D-1, H-1, W-1]
             # x2 [N, C, D,   H,   W  ]
             p_d = x2.shape[2] - x1.shape[2]
@@ -127,9 +118,9 @@ class UpConv(nn.Module):
             pad = nn.ConstantPad3d((0, p_w, 0, p_h, 0, p_d), 0)
             x1 = pad(x1)
 
-        x = torch.cat((x1, x2), 1)
-        x1 = self.conv_N_time(x)
-        x = x1 + x
+        # x = torch.cat((x1, x2), 1)
+        x = x1 + x2
+        x = self.conv_N_time(x)
         return x
 
 
@@ -146,10 +137,13 @@ class ConvNTimes(nn.Module):
         self.convs = nn.ModuleList()
         self.batchnorms = nn.ModuleList()
 
-        self.activation = activation_fn()
         for _ in range(N):
-            conv = nn.Conv3d(channel_num, channel_num, kernel_size=kernel_size,
-                             padding=kernel_size // 2)
+            conv = nn.Conv3d(
+                channel_num,
+                channel_num,
+                kernel_size=kernel_size,
+                padding=kernel_size // 2,
+            )
             self.convs.append(conv)
             norm = nn.BatchNorm3d(channel_num)
             self.batchnorms.append(norm)
@@ -157,8 +151,8 @@ class ConvNTimes(nn.Module):
     def forward(self, x):
         for conv, batchnorm in zip(self.convs, self.batchnorms):
             x = conv(x)
-            x = self.activation(x)
             x = batchnorm(x)
+            x = F.relu(x)
         return x
 
 
@@ -171,15 +165,18 @@ class Duplicate(nn.Module):
 
     def __init__(self, input_channel, duplication_num, kernel_size):
         super(Duplicate, self).__init__()
-        self.duplicate = nn.Conv3d(input_channel, duplication_num,
-                                   kernel_size=kernel_size, padding=kernel_size // 2)
-        self.activation = activation_fn()
+        self.duplicate = nn.Conv3d(
+            input_channel,
+            duplication_num,
+            kernel_size=kernel_size,
+            padding=kernel_size // 2,
+        )
         self.batch_norm = nn.BatchNorm3d(duplication_num)
 
     def forward(self, inp):
         x = self.duplicate(inp)
-        x = self.activation(x)
         x = self.batch_norm(x)
+        x = F.relu(x)
         return x
 
 